1c3aff9dcb78847f652eb21ac5cdb245b6588e40
multiplaie <multiplaie@protonmail.com>
2021-10-28 15:21:11 +0200

    pass music to play in params

--- src/routes/VlcController.js
+++ src/routes/VlcController.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const VLC = require('node-vlc-json');
-
 const player = new VLC({httpPort: '8080', httpPassword: 'pwd'});
 
-
-
-
 router.get('/play', (req, res) => {
-    player.play('/home/ubuntu/ambiance.mp3', function() {
+
+    player.play(req.query.path, function() {
         player.status(function(res) {
           console.log(res.state);
         });
-      });
-});
+    });
 
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ "status": "play" }));
+});
 
 module.exports = router;
